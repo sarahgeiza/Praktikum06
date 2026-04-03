631c89c2fa05b0a140e20f841abd83a96ccca721
sarahgeiza <sarahgeizask@gmail.com>
2026-04-03 10:11:32 +0700

    Praktikum 2 - Mengurutkan Data Mahasiswa Berdasarkan IPK (Bubble Sort)

---
+++ Mahasiswa25.java
@@ -0,0 +1,25 @@
+public class Mahasiswa25 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    // Konstruktor default
+    Mahasiswa25() {
+    }
+
+    // Konstruktor berparameter (dibuat ada yang nama var parameter inputnya sama ada yang tidak)
+    Mahasiswa25(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
---
+++ MahasiswaBerprestasi25.java
@@ -0,0 +1,32 @@
+public class MahasiswaBerprestasi25 {
+    Mahasiswa25 [] listMhs = new Mahasiswa25[5];
+    int idx;
+
+    void tambah (Mahasiswa25 m) {
+        if (idx<listMhs.length) {
+            listMhs[idx]=m;
+            idx++;
+            } else {
+                System.out.println("data sudah penuh");
+        }
+    }
+
+    void tampil (){
+        for (Mahasiswa25 m:listMhs) {
+            m.tampilInformasi();
+            System.out.println("----------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i=0; i<listMhs.length-1; i++) {
+            for (int j=1; j<listMhs.length-i; j++) {
+                if (listMhs[j].ipk>listMhs[j-1].ipk) {
+                    Mahasiswa25 tmp = listMhs[j];
+                    listMhs[j]=listMhs[j-1];
+                    listMhs[j-1]=tmp;
+                }
+            }
+        }
+    }
+}
